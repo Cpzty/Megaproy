0e7d49987044f6de0aa9a00e2f91f08fa68cc8b0
Cpzty <Cristianpzty@gmail.com>
2020-07-07 09:29:14 -0600

    modelo cuestionario

--- retos/models.py
+++ retos/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
 class Profile(models.Model):
@@ -26,3 +26,58 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+class Cuestionario(models.Model):
+    name = models.CharField(max_length=1000)
+    questions_count = models.IntegerField(default=0)
+    description = models.CharField(max_length=70)
+    created = models.DateTimeField(auto_now_add=True,null=True,blank=True)
+    slug = models.SlugField()
+    roll_out = models.BooleanField(default=False)
+class Meta:
+    ordering = ['created']
+    verbose_name_plural ="Cuestionarios"
+    def __str__(self):
+        return self.name
+
+class Pregunta(models.Model):
+    cuestionario = models.ForeignKey(Cuestionario, on_delete=models.CASCADE)
+    label = models.CharField(max_length=1000)
+    order = models.IntegerField(default=0)
+    def __str__(self):
+        return self.label
+
+class Respuesta(models.Model):
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000)
+    is_correct = models.BooleanField(default=False)
+    def __str__(self):
+        return self.text
+
+class CuestionarioRespondido(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Cuestionario, on_delete=models.CASCADE)
+    correct_answers = models.IntegerField(default=0)
+    completed = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.user.username
+class Respuesta_final(models.Model):
+    respondido = models.ForeignKey(CuestionarioRespondido, on_delete=models.CASCADE)
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+    respuesta = models.ForeignKey(Respuesta,on_delete=models.CASCADE,null=True,blank=True)
+    def __str__(self):
+        return self.question.label
+
+    @receiver(post_save, sender=Cuestionario)
+    def set_default_quiz(sender, instance, created,**kwargs):
+    cuestionario = Cuestionario.objects.filter(id = instance.id)
+    cuestionario.update(questions_count=instance.pregunta_set.filter(cuestionario=instance.pk).count())
+    @receiver(post_save, sender=Pregunta)
+    def set_default(sender, instance, created,**kwargs):
+    cuestionario = Cuestionario.objects.filter(id = instance.cuestionario.id)
+    cuestionario.update(questions_count=instance.cuestionario.question_set.filter(cuestionario=instance.cuestionario.pk).count())
+    @receiver(pre_save, sender=Cuestionario)
+    def slugify_title(sender, instance, *args, **kwargs):
+     instance.slug = slugify(instance.name)
+
