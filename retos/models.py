4612045d910a35e72fa8192ed6c0c63dd53d98b2
Cpzty <Cristianpzty@gmail.com>
2020-08-15 05:36:05 -0600

    foreignkey test

--- retos/models.py
+++ retos/models.py
@@ -5,7 +5,7 @@ from django.dispatch import receiver
 from django.template.defaultfilters import slugify
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None, unique=False)
+    user = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=1)
     racha = models.IntegerField(default=0)
     #caritas
     alegre = models.IntegerField(default=0)
