bdf3731d6358c55b006fa2ba4c3bad6fd879c92d
Cpzty <Cristianpzty@gmail.com>
2020-12-05 19:16:29 -0600

    update cryptography

--- proyecto_psicologia/settings.py
+++ proyecto_psicologia/settings.py
@@ -22,11 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8zb$lhdi_7n^-h_67o9+f+tc!=8sfq0nqo@75#_m5-%1#=-rvv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
