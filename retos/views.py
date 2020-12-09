d5d66dfd87a351687d44548584d19afd5232c9a6
Cpzty <Cristianpzty@gmail.com>
2020-12-08 22:04:47 -0600

    unexpected cuestionario fix

--- retos/views.py
+++ retos/views.py
@@ -53,9 +53,9 @@ class PreguntasView(APIView):
 
     def post(self, request):
         cuestionarios = Cuestionarios.objects.filter(titulo=request.POST.get('titulo', 'default'))
-        serializer = PreguntasSerializer(cuestionario = cuestionarios, data=request.data)
+        serializer = PreguntasSerializer(data=request.data)
         if serializer.is_valid(raise_exception=ValueError):
-            serializer.create(validated_data=serializer.data)
+            serializer.create(cuestionario = cuestionarios,validated_data=serializer.data)
             return Response(
                 serializer.data,
                 status=status.HTTP_201_CREATED
