252cdd2c957d760d673097a4aaa77bc50d2c60f4
Cpzty <Cristianpzty@gmail.com>
2020-12-11 06:09:02 -0600

    ordenar usuarios por cantidad de comentarios

--- retos/views.py
+++ retos/views.py
@@ -876,6 +876,19 @@ class ProfileRecordView(APIView):
                 data['username' + str(i)] = username
                 data['insignias' + str(i)] = cantidad_insignias
 
+        elif ranks == 'comentarios':
+            ids_usuarios = []
+            comentarios = Comentarios.objects.all()
+            for user in comentarios:
+                if user.user_id not in ids_usuarios:
+                    ids_usuarios.append(user.user_id)
+            for i in range(len(ids_usuarios)):
+                comentario = Comentarios.objects.filter(user_id=ids_usuarios[i]).count()
+                username = User.objects.get(id=ids_usuarios[i]).username
+                data['username' + str(i)] = username
+                data['comentarios' + str(i)] = comentario
+
+
         elif ranks == 'retos':
             ids_usuarios = []
             usuarios_retos = Reto_finalizado.objects.all()
