ee8bc76cd292230914bced4034ffb2f248d77872
Cpzty <Cristianpzty@gmail.com>
2020-12-11 06:42:44 -0600

    typo

--- retos/views.py
+++ retos/views.py
@@ -891,7 +891,7 @@ class ProfileRecordView(APIView):
             sort_data2 = sorted(data2.items(), key=lambda x: x[1][1], reverse=True)
             for i in range(len(ids_usuarios)):
                 data['username' + str(i)] = sort_data2[1][1][1]
-                data['username' + str(i)] = sort_data2[1][1][0]
+                data['comentarios' + str(i)] = sort_data2[1][1][0]
 
 
         elif ranks == 'retos':
